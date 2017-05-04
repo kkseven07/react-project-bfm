3642b3d01f064714c3dc00d86e4a2fac1687cb2f
IlyasM <ilyaskz7@gmail.com>
2017-05-04 09:21:45 +0600

    cleared build folder

--- public/[chunkFilename]-53e76a6474630fb210a7.async.js
+++
@@ -1,2 +0,0 @@
-/*! Copyright Ilyas Malgazhdarov */
-webpackJsonp([0],{499:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=n(1),o=function(e){return e&&e.__esModule?e:{default:e}}(u);n(500),t.default=function(e){return o.default.createElement("div",{style:{fontFamily:"BebasBold"}},"About")}},500:function(e,t){}});
--- public/async-c77bbb30ae18f9c81788.async.js
+++
@@ -1,2 +0,0 @@
-/*! Copyright Ilyas Malgazhdarov */
-webpackJsonp([0],{508:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=n(1),o=function(e){return e&&e.__esModule?e:{default:e}}(u);n(509),t.default=function(e){return o.default.createElement("div",{style:{fontFamily:"BebasBold"}},"About")}},509:function(e,t){}});
