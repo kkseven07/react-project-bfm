e8802e9f86e7442ce07b60c9d87a5ab98c69c158
IlyasM <ilyaskz7@gmail.com>
2017-05-05 19:34:49 +0600

    removed gallery

--- src/app/about/index.js
+++ src/app/about/index.js
@@ -1,3 +1,3 @@
 import React from 'react'
 import './about.css'
-export default (props)=>(<div style={{fontFamily:"BebasBold"}}>About</div>)
+export default (props)=>(<div style={{fontFamily:"Agaaler"}}>About</div>)
