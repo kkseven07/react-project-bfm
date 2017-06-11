77c8115c4c67c4b9be5864cdd9cfef4bd18ba820
IlyasM <ilyaskz7@gmail.com>
2017-06-11 11:51:40 +0600

    latest

--- src/app/home/components/gallery.js
+++ src/app/home/components/gallery.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./gallery.css";
 import Slider from "react-slick";
-import Banner from '../../../../assets/img/banner.jpg'
+import url from "../../../entry/url";
+import Banner from "../../../../assets/img/banner.jpg";
 export default props => {
     return (
-        <div className="width-full flex flex-center" styleName="r">
-        <div onClick={props.scrollTo} styleName="start-btn">Начать</div>
-            <div styleName="text" >СОЗДАЙ НЕЗАБЫВАЕМЫЙ ПОДАРОК</div>
+        <div
+            className="width-full flex flex-center"
+            style={{
+                backgroundImage: `url(${url}/images/cdn/banner.jpg)`
+            }}
+            styleName="r"
+        >
+            <div onClick={props.scrollTo} styleName="start-btn">Начать</div>
+            <div styleName="text">СОЗДАЙ НЕЗАБЫВАЕМЫЙ ПОДАРОК</div>
         </div>
     );
 };
