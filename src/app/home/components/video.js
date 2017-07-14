b4064e74659a37dc13ba5c67b7e9fd5dd1c65e63
Konstantin Kim <kkimseven@gmail.com>
2017-07-14 10:39:45 +0600

    some fixes

--- src/app/home/components/video.js
+++ src/app/home/components/video.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import './video.css'
-export default (props)=>{
+import ReactCSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
+class Video extends React.Component {
+	state={style:'none', style1:'1'}
+	render() {
 
-
-    return <div className="width-full flex flex-center" styleName="r">
-
-        <iframe styleName="youtube" src="https://www.youtube.com/embed/APe8-aG9hw8" allowFullScreen ></iframe>
-
-    </div>
+	    return <div className="width-full flex flex-center" styleName="r">
+			<div styleName="title">КАК ЭТО РАБОТАЕТ?</div>
+	        	<div 
+	        		styleName="youtube" 
+	        		// onMouseEnter={()=>{this.setState({style:'block', style1:'130'}); console.log("on")}}
+	        		// onMouseLeave={()=>{this.setState({style:'none', style1:'1'}); console.log("off")}} 
+	        		style={{zIndex:this.state.style1}}
+	        	>
+	        		<iframe  style={{border:'none', width:'100%', height:'100%'}}src="https://www.youtube.com/embed/APe8-aG9hw8" allowFullScreen ></iframe>
+	        		
+	        	</div>
+				<div styleName="darkness" style={{display:this.state.style}}></div>
+	    </div>
+	}
 }
+export default Video;
