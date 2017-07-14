b388d501c961aeccdee91d61898f7a7325b920a6
IlyasM <ilyaskz7@gmail.com>
2017-07-14 11:29:48 +0600

    passd

--- src/app/home/components/video.js
+++ src/app/home/components/video.js
@@ -1,23 +1,34 @@
-import React from 'react'
-import './video.css'
-import ReactCSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
+import React from "react";
+import "./video.css";
+import ReactCSSTransitionGroup from "react-transition-group/CSSTransitionGroup";
 class Video extends React.Component {
-	state={style:'none', style1:'1'}
+	state = { style: "none", style1: "1" };
 	render() {
+		return (
+			<div className="width-full flex flex-center" styleName="r">
+				<div
+					styleName="youtube"
+					// onMouseEnter={()=>{this.setState({style:'block', style1:'130'}); console.log("on")}}
+					// onMouseLeave={()=>{this.setState({style:'none', style1:'1'}); console.log("off")}}
+					style={{ zIndex: this.state.style1 }}
+				>
+					<iframe
+						style={{
+							border: "none",
+							width: "100%",
+							height: "100%"
+						}}
+						src="https://www.youtube.com/embed/APe8-aG9hw8"
+						allowFullScreen
+					/>
 
-	    return <div className="width-full flex flex-center" styleName="r">
-			<div styleName="title">КАК ЭТО РАБОТАЕТ?</div>
-	        	<div 
-	        		styleName="youtube" 
-	        		// onMouseEnter={()=>{this.setState({style:'block', style1:'130'}); console.log("on")}}
-	        		// onMouseLeave={()=>{this.setState({style:'none', style1:'1'}); console.log("off")}} 
-	        		style={{zIndex:this.state.style1}}
-	        	>
-	        		<iframe  style={{border:'none', width:'100%', height:'100%'}}src="https://www.youtube.com/embed/APe8-aG9hw8" allowFullScreen ></iframe>
-	        		
-	        	</div>
-				<div styleName="darkness" style={{display:this.state.style}}></div>
-	    </div>
+				</div>
+				<div
+					styleName="darkness"
+					style={{ display: this.state.style }}
+				/>
+			</div>
+		);
 	}
 }
 export default Video;
