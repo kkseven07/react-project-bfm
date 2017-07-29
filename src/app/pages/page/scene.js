b3f96b133571e29fb670dd3da47a7458842bd35a
Konstantin Kim <kkimseven@gmail.com>
2017-07-29 23:18:47 +0600

    kiko: builder page bug: long name

--- src/app/pages/page/scene.js
+++ src/app/pages/page/scene.js
@@ -3,6 +3,6 @@ import './scene.css'
 
 export default ({book}) =>(
     <div className="full" styleName="r">
-        <div styleName="text-name">{book.name} </div>
+        <div styleName="text-name" style={{fontSize:book.name.indexOf(" ")>-1&&'2.5em'}}>{book.name} </div>
     </div>
 );
