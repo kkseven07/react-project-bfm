f423fdf54293a99e66ca2da383a8c458f9b0c930
Konstantin Kim <kkimseven@gmail.com>
2017-08-08 16:35:23 +0600

    no paspartu

--- src/app/shared/components/paspartu.js
+++ src/app/shared/components/paspartu.js
@@ -9,14 +9,10 @@ const Paspartu =({name, page})=> {
 	return (
 		<div
 			styleName="r"
-			style={{color:page.data.text_color,
-				boxShadow:`0 -1px 5px ${page.data.border_color}, 0 1px 5px ${page.data.border_color},
-				1px 0 5px ${page.data.border_color}, -1px 0 5px ${page.data.border_color}`
-			}}
 		>
 			<div 
 				styleName="part1" 
-				style={{border:`0.072em solid ${page.data.border_color}`, fontSize:fontsize}}
+				style={{fontSize:fontsize}}
 			>
 				{nameToRender} 
 			</div>
